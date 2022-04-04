cff3796fbc5f376471d7afc7765f5abdc68f4588
Tony M <tony@fbpllc.com>
2022-04-04 15:52:48 -0700

    bump og version

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import { ButtonStyles, useTheme as OgUseTheme } from '@opensesame/gemini';
+import {
+  createTheme,
+  ThemeOptions,
+  ThemeProvider,
+  styled
+} from '@mui/material/styles';
+import cssBaseline from '../node_modules/@opensesame/gemini/styles/cssBaseline';
+import provideTokens from '../node_modules/@opensesame/gemini/styles/provideTokens';
+import useMuiTheme from '../node_modules/@opensesame/gemini/styles/useMuiTheme';
 import { AppBar, Button } from '@mui/material';
 
-const ogTheme = OgUseTheme() as any;
-
-const buttonStyles = ButtonStyles(OgUseTheme()) as any;
-
-delete buttonStyles.root.backgroundColor;
-delete buttonStyles.root.color;
-delete buttonStyles.root['&:hover'];
-
-ogTheme.components = {
-  MuiButton: {
-    styleOverrides: {
-      root: buttonStyles.root
-    }
-  }
-};
-
-delete ogTheme.spacing;
-
-const theme = createTheme(ogTheme);
-
-const AppBarPrimary = styled(AppBar)({
-  padding: theme.spacing(3)
-});
-
-const AppWrapper = styled('div')({
-  paddingLeft: theme.spacing(3),
-  paddingRight: theme.spacing(3),
-  paddingTop: theme.spacing(10)
-});
-
 function App() {
+  provideTokens('simon');
+  //console.log(useMuiTheme);
+
+  const ogTheme = useMuiTheme() as ThemeOptions; // <-- Gemini function returns an OG type, MuiThemeOptions, that we cast to a MUI type
+
+  const theme = createTheme(ogTheme); // <-- MUI function to create a MUI theme
+
+  cssBaseline(); // <-- Gemini baseline (MUI baseline is not used)
+
+  const AppBarPrimary = styled(AppBar)({
+    padding: theme.spacing(3)
+  });
+
+  const AppWrapper = styled('div')({
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    paddingTop: theme.spacing(10)
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <AppWrapper>
